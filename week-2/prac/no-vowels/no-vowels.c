7d5be66097f84c530aad964d5c4fccd74e9a5650
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-11 06:31:23 +0000

    Wed, May 10, 2023, 11:31 PM -07:00

--- week-2/prac/no-vowels/no-vowels.c
+++ week-2/prac/no-vowels/no-vowels.c
@@ -30,51 +30,53 @@ string replace(string input)
 
     string replaced;
 
-    for (int i = 0; i < len; i++)
-    {
-        // Replace uppercase letters with lowercase
-        if (isupper(argv[1] == 'A'))
-        {
-            argv[i] = 'A' + 32;
-            return 0;
-        }
-        else if (isupper(char argv[1] == 'E'))
-        {
-            argv[i] = 'E' + 32;
-            return 0;
-        }
-        else if (isupper(char argv[1] == 'I'))
-        {
-            argv[i] = 'I' + 32;
-            return 0;
-        }
-        else if (isupper(char argv[1] == 'O'))
-        {
-            argv[i] = 'O' + 32;
-            return 0;
-        }
+    // for (int i = 0; i < len; i++)
+    // {
+    //     // Replace uppercase letters with lowercase
+    //     if (isupper(argv[1] == 'A'))
+    //     {
+    //         argv[i] = 'A' + 32;
+    //         return 0;
+    //     }
+    //     else if (isupper(char argv[1] == 'E'))
+    //     {
+    //         argv[i] = 'E' + 32;
+    //         return 0;
+    //     }
+    //     else if (isupper(char argv[1] == 'I'))
+    //     {
+    //         argv[i] = 'I' + 32;
+    //         return 0;
+    //     }
+    //     else if (isupper(char argv[1] == 'O'))
+    //     {
+    //         argv[i] = 'O' + 32;
+    //         return 0;
+    //     }
 
-        if (argv[i] == 'a')
-        {
-            argv[i] = '54';
-            return 0;
-        }
-        if (argv[i] == 'e')
-        {
-            argv[i] = '51';
-            return 0;
-        }
-        if (argv[i] == 'i')
-        {
-            argv[i] = '49';
-            return 0;
-        }
-        if (argv[1] == 'o')
-        {
-            argv[i] = '48';
-            return 0;
-        }
-    }
+    //     if (argv[i] == 'a')
+    //     {
+    //         argv[i] = '54';
+    //         return 0;
+    //     }
+    //     if (argv[i] == 'e')
+    //     {
+    //         argv[i] = '51';
+    //         return 0;
+    //     }
+    //     if (argv[i] == 'i')
+    //     {
+    //         argv[i] = '49';
+    //         return 0;
+    //     }
+    //     if (argv[1] == 'o')
+    //     {
+    //         argv[i] = '48';
+    //         return 0;
+    //     }
+    // }
 
-    return replace;
+    // return replace; or return replaced;
+    return 0;
+
 }
