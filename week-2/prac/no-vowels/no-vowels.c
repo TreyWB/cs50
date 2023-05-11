718c21d49bd0be85a9771af42160adf09d806e1b
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-11 05:23:10 +0000

    Wed, May 10, 2023, 10:23 PM -07:00

--- week-2/prac/no-vowels/no-vowels.c
+++ week-2/prac/no-vowels/no-vowels.c
@@ -6,6 +6,8 @@
 #include <cs50.h>
 #include <stdio.h>
 
+string replace(string argv[1]);
+
 int main(int argc, string argv[])
 {
     if (argc != 1)
@@ -13,5 +15,19 @@ int main(int argc, string argv[])
         printf("Usage: ./no-vowels word\n");
         return 1;
     }
-    
+
+    printf("%s\n", replace(string argv[1]));
+}
+
+string replace(string argv[1])
+{
+    int len;
+    len = argv[1];
+
+    string replaced;
+
+    for (i = 0; i < len; i++)
+    {
+        
+    }
 }
