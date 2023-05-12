3f9c0871efec4369571ac666968ecf5613062c08
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-12 01:49:09 +0000

    Thu, May 11, 2023, 6:49 PM -07:00

--- week-1/pset/credit/test.c
+++ week-1/pset/credit/test.c
@@ -18,10 +18,27 @@ int main(void)
     multiplied[0] = 3;
     multiplied[1] = 12;
     multiplied[2] = 5;
+    int stage1_sum;
+        stage1_sum = 0;
 
     // Saving each digit in cc_num seperately, into the digit[] array
     for (int i = 0; i < 2; i++)
     {
+        int split1;
+        split1 = 0;
+        int split2;
+        split2 = 0;
+
+        if (multiplied[i] > 9)
+        {
+            split1 = multiplied[i] / 10;
+            split2 = multiplied[i] % 10;
+        }
         stage1_sum += multiplied[i];
+        stage1_sum += split1;
+        stage1_sum += split2;
+        printf("Split 1: %i\n", split1);
+        printf("Split 2: %i\n", split2);
+        printf("Total: %i\n", stage1_sum);
     }
 }
