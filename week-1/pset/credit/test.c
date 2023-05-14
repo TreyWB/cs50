4f380877dbe872c8b4118466c813558920ce2c96
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-14 01:15:10 +0000

    Sat, May 13, 2023, 6:15 PM -07:00

--- week-1/pset/credit/test.c
+++ week-1/pset/credit/test.c
@@ -7,71 +7,23 @@ int main(void)
     string cc_num = get_string("Number: ");
     // printf("Output: %s\n", cc_num);
 
-    // Getting cc_num length
-    int len = strlen(cc_num);
-    printf("Length: %i\n", len);
-
-    // Defining cc_num array, automatically defines length of array based on cc_num_length
-    int digit[len];
-
-    int multiplied[3];
-    multiplied[0] = 3;
-    multiplied[1] = 12;
-    multiplied[2] = 5;
-    int stage1_sum;
-    stage1_sum = 0;
-
-
-
-
-
-
-
-
-
-    // Saving each digit in cc_num seperately, into the digit[] array
-    for (int i = 0; i < 2; i++)
-    {
-        int split1;
-        split1 = 0;
-        int split2;
-        split2 = 0;
-
-        if (multiplied[i] <= 9)
-        {
-            stage1_sum += multiplied[i];
-        }
-
-        else if (multiplied[i] > 9)
-        {
-            split1 = multiplied[i] / 10;
-            split2 = multiplied[i] % 10;
-            stage1_sum += split1;
-            stage1_sum += split2;
-        }
-
-        printf("Split 1: %i\n", split1);
-        printf("Split 2: %i\n", split2);
-        printf("Total: %i\n", stage1_sum);
-    }
+    int sum_every_other_digit = every_other_digit(cc_num);
 }
 
+int every_other_digit(string cc_num)
+{
+    int len = strlen(cc_num);
 
+    long digit;
+    digit = cc_num;
 
-
-
-
-
-
-
-
-
-
-        // if (multiplied[i] > 9)
-        // {
-        //     split1 = multiplied[i] / 10;
-        //     split2 = multiplied[i] % 10;
-        // }
-        // stage1_sum += multiplied[i];
-        // stage1_sum += split1;
-        // stage1_sum += split2;
+    int sum = 0;
+    bool isAlternateDigit = false;
+    while (cc_num > 0)
+    {
+        if (isAlternateDigit == true)
+        {
+            int last_digit =
+        }
+    }
+}
