bca93c840c61592df3978f2b646600b88613ceef
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-14 01:21:54 +0000

    Sat, May 13, 2023, 6:21 PM -07:00

--- week-1/pset/credit/test.c
+++ week-1/pset/credit/test.c
@@ -2,22 +2,18 @@
 #include <stdio.h>
 #include <string.h>
 
+int every_other_digit(long cc_num);
+
 int main(void)
 {
-    string cc_num = get_string("Number: ");
+    long cc_num = get_long("Number: ");
     // printf("Output: %s\n", cc_num);
 
-    int sum_every_other_digit;
-    every_other_digit(cc_num);
+    int sum_every_other_digit = every_other_digit(cc_num);
 }
 
-int every_other_digit(string cc_num)
+int every_other_digit(long cc_num)
 {
-    int len = strlen(cc_num);
-
-    long digit;
-    digit = cc_num;
-
     int sum1 = 0;
     bool isAlternateDigit = false;
     while (cc_num > 0)
@@ -27,7 +23,7 @@ int every_other_digit(string cc_num)
             int last_digit = cc_num % 10;
             sum1 += last_digit;
         }
-        isAlternateDigit = !isAlternateDigit
+        isAlternateDigit = !isAlternateDigit;
         cc_num = cc_num / 10;
 
     }
