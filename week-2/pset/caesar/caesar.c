b97b108cd181112bccade1b0e37972d8a77c4c53
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-15 20:48:01 +0000

    Mon, May 15, 2023, 1:48 PM -07:00

--- week-2/pset/caesar/caesar.c
+++ week-2/pset/caesar/caesar.c
@@ -1,9 +1,21 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <ctype.h>
+#include <string.h>
 
 int main(int argc, string argv[])
 {
-    if (argv[1] )
+    int len = strlen(argv[1]);
+
+    for (int i = 0; i < len; i++)
+    {
+        int c = argv[i];
+    }
+
+    if (isdigit(c) = 0)
+    {
+        printf("Usage: ./caesar key\n");
+    }
 
     printf("plaintext: %s\n", argv[1]);
 }
