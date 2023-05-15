4b435a60ef3a708a63f7e198d635eb42bb83c4e9
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-15 20:49:10 +0000

    Mon, May 15, 2023, 1:49 PM -07:00

--- week-2/pset/caesar/caesar.c
+++ week-2/pset/caesar/caesar.c
@@ -7,12 +7,7 @@ int main(int argc, string argv[])
 {
     int len = strlen(argv[1]);
 
-    for (int i = 0; i < len; i++)
-    {
-        int c = argv[i];
-    }
-
-    if (isdigit(c) = 0)
+    if (isdigit() = 0)
     {
         printf("Usage: ./caesar key\n");
     }
