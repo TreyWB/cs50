525bdfe096315d469d646a4ef9a5615ece649c90
TreyWB <132207320+TreyWB@users.noreply.github.com>
2023-05-15 04:29:45 +0000

    Sun, May 14, 2023, 9:29 PM -07:00

--- week-2/pset/bulbs/test.c
+++ week-2/pset/bulbs/test.c
@@ -7,8 +7,25 @@ int main(void)
     int input = get_int("Decimal: ");
 
     // Initialize product array
-    int product[];
+    int product[6];
 
-    // Math to convert to binary
+    while (input > 0)
+    {
+        for (int i = 0; i < 6; i++)
+        {
+            
+        }
+        // Math to convert to binary
+        int remainder1 = input % 2;
+
+        if (remainder1 != 0)
+        {
+            product[0] = 1;
+        }
+        else
+        {
+            product[0] = 0;
+        }
+    }
 
 }
